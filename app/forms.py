d03900e7346d9287e145b364f15f02931f5811c2
Kayla Imming <kaylaimming@gmail.com>
2023-03-01 16:38:03 -0800

    Add RegistrationForm class with wtforms

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField, EmailField
 from wtforms.validators import DataRequired
 
 class PokemonNameForm(FlaskForm):
     pokemon_name = StringField('Pokemon Name:', validators=[DataRequired()])
     submit_btn = SubmitField('Let\'s go!')
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name:', validators=[DataRequired()])
+    last_name = StringField('Last Name:', validators=[DataRequired()])
+    email = EmailField('Email:', validators=[DataRequired()])
+    password = PasswordField('Password:', validators=[DataRequired()])
+    submit_btn = SubmitField('Login')
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email:', validators=[DataRequired()])
+    password = PasswordField('Password:', validators=[DataRequired()])
+    submit_btn = SubmitField('Login')
