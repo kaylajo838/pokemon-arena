280b02529e93722477fce654df9b0f4f71ab7731
Kayla Imming <kaylaimming@gmail.com>
2023-03-02 20:10:07 -0800

    Add sqlalchemy_database_uri and sqlalchemy_track_modifications

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ import os
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    REGISTERED_USERS = {}
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
