2a9d21a65eaf473f740657ce715731582d74eb13
Kayla Imming <kaylaimming@gmail.com>
2023-03-02 19:48:12 -0800

    Add imports for flask_login sqlalchemy flask_migrate, add login registering packages, add data manager db and migrate, add all configure login settings login_view login_message login_messages_category, add model import from app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+# registering packages
+login = LoginManager(app)
+
+# data manager
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# configure login settings
+login.login_view = 'login'
+login.login_message = 'You must be logged in to see this page.'
+login.login_message_category = 'warning'
+
+from app import routes, models
