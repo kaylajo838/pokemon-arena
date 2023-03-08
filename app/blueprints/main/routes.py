4d3e88ea2cd53cb10f565be2724bf63579523e62
Kayla Imming <kaylaimming@gmail.com>
2023-03-08 14:26:26 -0800

    Added syntax to show flash messages when a pokemon is already on your team, flash message for when a pokemon doesn't exist, changed that render_template to redirect, add and elif to see if pokemon is greater than or equal to 5, added a count to first if statement to count if pokemon is less than 5, added captured_pokemon variable, added more imports to fit page

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 import requests
 from app.blueprints.main import main
 from app.blueprints.auth.forms import PokemonNameForm
 from flask_login import login_required
+from ...models import Captured
 
 @main.route('/')
 @login_required
@@ -14,13 +15,14 @@ def home():
 @login_required
 def pokemon_form():
     form = PokemonNameForm()
-    if request.method == 'POST' and form.validate_on_submit():
+    captured_pokemon = Captured.query.all()
+    if request.method == 'POST' and form.validate_on_submit() and Captured.query.count() < 5:
         name = form.pokemon_name.data.lower()
         pokemon_url = f"https://pokeapi.co/api/v2/pokemon/{name}"
         pokemon_response = requests.get(pokemon_url)
+        pokemon_data = []
         if pokemon_response.ok:
             pokemon_name = pokemon_response.json()
-            new_pokemon_data = []
             pokemon_dict = {
                 "name": pokemon_name["forms"][0]["name"],
                 "ability": pokemon_name["abilities"][0]["ability"]["name"],
@@ -30,9 +32,28 @@ def pokemon_form():
                 "hp_base_stat": pokemon_name["stats"][0]["base_stat"],
                 "defense_base_stat": pokemon_name["stats"][2]["base_stat"]
             }
-            new_pokemon_data.append(pokemon_dict)
-            return render_template('pokemon_form.html', new_pokemon_data=new_pokemon_data, form=form)
+
+            poke_name = Captured.query.filter_by(name=pokemon_dict['name']).first()
+            if poke_name:
+                flash(f'That pokemon is already on your team! Please choose another.', 'danger')
+                return redirect(url_for('main.pokemon_form'))
+        
+            pokemon_data.append(pokemon_dict)
+
+            new_pokemon = Captured()
+
+            new_pokemon.from_dict(pokemon_dict)
+
+            new_pokemon.save_to_db()
+
+            return redirect(url_for('main.pokemon_form'))
+         
         else:
-            error = "That pokemon doesn't exist."
-            return render_template('pokemon_form.html', error=error, form=form)
-    return render_template('pokemon_form.html', form=form)
+            flash('That pokemon doesn\'t exist', 'danger')
+            return redirect(url_for('main.pokemon_form'))
+    elif request.method == 'POST' and form.validate_on_submit() and Captured.query.count() >= 5:
+        flash('You already have 5 Pokemon on your team!', 'danger')
+        return render_template('pokemon_form.html', form=form, captured_pokemon=captured_pokemon)
+    return render_template('pokemon_form.html', form=form, captured_pokemon=captured_pokemon)
+
+
