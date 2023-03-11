480b8162e66032bc7fa04f056671fb153b093db9
Kayla Imming <kaylaimming@gmail.com>
2023-03-10 21:21:03 -0800

    Add battle_queue and battle_team routes with logic to fight pokemon

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -112,8 +112,84 @@ def view_team():
 
 # ***************************DON'T TOUCH THE CODE ABOVE THIS LINE****************************************
 
+@main.route('/battle_queue')
+@login_required
+def battle_queue():
+    users = User.query.all()
+    return render_template('battle_pokemon.html', users=users)
+
+@main.route('/battle_team/<user_id>')
+@login_required
+def battle_team(user_id):
+    user = User.query.get(user_id)
+    opponent_team = user.captured_pokemon
+    my_team = current_user.captured_pokemon
+
+    # if opponent_team.count() < 5:
+    #     flash(f'Oh no! {user.first_name} doesn\'t have enough pokemon to battle!', 'danger')
+    #     redirect(url_for('main.battle_queue'))
+    # elif my_team.count() < 5:
+    #     flash(f'Oh no! You don\'t have enough pokemon to battle!', 'danger')
+    #     redirect(url_for('main.battle_queue'))
+
+
+    opponent_attack = []
+    self_attack = []
+
+    opponent_defense = []
+    self_defense = []
+
+    opponent_hp =[]
+    self_hp = []
+
+    for opponent_attack_play in opponent_team:
+        opponent_attack.append(opponent_attack_play.attack_base_stat)
+    for self_attack_play in my_team:
+        self_attack.append(self_attack_play.attack_base_stat)
+    
+    for opponent_defense_play in opponent_team:
+        opponent_defense.append(opponent_defense_play.defense_base_stat)
+    for self_defense_play in my_team:
+        self_defense.append(self_defense_play.defense_base_stat)
+    
+    for opponent_hp_play in opponent_team:
+        opponent_hp.append(opponent_hp_play.defense_base_stat)
+    for self_hp_play in my_team:
+        self_hp.append(self_hp_play.defense_base_stat)
+
+
+    opponent_total_attack = sum(opponent_attack)
+    self_total_attack = sum(self_attack)
+    
+    opponent_total_defense = sum(opponent_defense)
+    self_total_defense = sum(self_defense)
+    
+    opponent_total_hp = sum(opponent_hp)
+    self_total_hp = sum(self_hp)
+
+    # self against opponent
+    if opponent_total_attack - self_total_defense < 0:
+        defense_against_opponent_attack = 0
+    else:
+        defense_against_opponent_attack = opponent_total_attack - self_total_defense
+    total_attack_from_opponenet = self_total_hp - defense_against_opponent_attack
+    
+    # opponent against self 
+    if self_total_attack - opponent_total_defense < 0:
+        defense_against_self_attack = 0
+    else:
+        defense_against_self_attack = self_total_attack - opponent_total_defense
+    total_attack_from_self = opponent_total_hp - defense_against_self_attack
+
+
+    if total_attack_from_opponenet > total_attack_from_self:
+        flash(f"{current_user.first_name}'s team wins the game with {total_attack_from_opponenet} points!", "success")
+    else:
+        flash(f"{user.first_name}'s tean wins the game with {total_attack_from_opponenet} points!", "danger")
+
+    return render_template('battle_team.html', opponent_team=opponent_team, my_team=my_team)
+
 @main.route('/battle')
 @login_required
 def battle():
-    users = User.query.all()
-    return render_template('battle_pokemon.html', users=users)
+    return render_template('battle.html')
